0b03b20a2069f618a5801dd1b7e27b099772d949
work_pc <18772570449@163.com>
2020-07-20 16:08:08 +0800

    frist commit

---
+++ test_lib/src/main/java/com/ls/test/test_lib/Upload.kt
@@ -0,0 +1,13 @@
+package com.ls.test.test_lib
+
+/**
+ * @ClassName: Upload
+ * @Description:
+ * @Author: ls
+ * @Date: 2020/7/20 15:45
+ */
+class Upload {
+    fun test(){
+        print("test")
+    }
+}
